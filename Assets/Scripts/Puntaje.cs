37c97afb454593870df591c2c09f23b49dc1ac8a
pepe <lihuito@gmail.com>
2025-11-23 23:17:05 -0300

    2 Nuevas Scenas, derrota y victoria.(intento de score)

---
+++ Assets/Scripts/Puntaje.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using TMPro;
+public class Puntaje : MonoBehaviour
+{
+    private float puntos;
+
+    private TextMeshProUGUI textMesh;
+
+    private void Start()
+    {
+        textMesh = GetComponent<TextMeshProUGUI>();
+    }
+
+    private void Update()
+    {
+        puntos += Time.deltaTime;
+        textMesh.text = puntos.ToString("0");
+    }
+
+    public void SumarPuntos(float puntosEntrando)
+    {
+        puntos += puntosEntrando;
+    }
+}
